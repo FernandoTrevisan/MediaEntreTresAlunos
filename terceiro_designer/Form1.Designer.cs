553d29bf7099845a8a631230891dd1cba31224b3
fernandotrevisan <fernando.trevisan01@etec.sp.gov.br>
2023-05-04 13:57:58 -0300

    Adicionar arquivos de projeto.

---
+++ terceiro_designer/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace terceiro_designer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.aprovacao = new System.Windows.Forms.Label();
+            this.box_resultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.button1.Location = new System.Drawing.Point(390, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label1.Location = new System.Drawing.Point(145, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nota 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label2.Location = new System.Drawing.Point(145, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nota 2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label3.Location = new System.Drawing.Point(145, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nota 3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.textBox3.Location = new System.Drawing.Point(257, 230);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(158, 38);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.textBox4.Location = new System.Drawing.Point(257, 186);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(158, 38);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(257, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 38);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.label4.Location = new System.Drawing.Point(30, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 55);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Média:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(65, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(426, 56);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Média dos alunos";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.label6.Location = new System.Drawing.Point(12, 515);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(254, 55);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Resultado:";
+            // 
+            // aprovacao
+            // 
+            this.aprovacao.AutoSize = true;
+            this.aprovacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.aprovacao.Location = new System.Drawing.Point(30, 570);
+            this.aprovacao.Name = "aprovacao";
+            this.aprovacao.Size = new System.Drawing.Size(0, 55);
+            this.aprovacao.TabIndex = 13;
+            this.aprovacao.Click += new System.EventHandler(this.aprovacao_Click);
+            // 
+            // box_resultado
+            // 
+            this.box_resultado.BackColor = System.Drawing.SystemColors.Control;
+            this.box_resultado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.box_resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.box_resultado.Location = new System.Drawing.Point(40, 442);
+            this.box_resultado.Name = "box_resultado";
+            this.box_resultado.Size = new System.Drawing.Size(172, 55);
+            this.box_resultado.TabIndex = 10;
+            this.box_resultado.TextChanged += new System.EventHandler(this.box_resultado_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(525, 650);
+            this.Controls.Add(this.aprovacao);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.box_resultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label aprovacao;
+        private System.Windows.Forms.TextBox box_resultado;
+    }
+}
+
---
+++ terceiro_designer/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace terceiro_designer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        int Nota1;
+        int Nota2;
+        int Nota3;
+
+        double resultado;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resultado = (Nota1 + Nota2 + Nota3) / 3;
+            box_resultado.Text = resultado.ToString("f2");
+
+            if (resultado < 7)
+                aprovacao.Text = "REPROVADO";
+
+            else
+                aprovacao.Text = "APROVADO";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Nota1 = int.Parse(textBox1.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            Nota2 = int.Parse(textBox4.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Nota3 = int.Parse(textBox3.Text);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void box_resultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aprovacao_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
